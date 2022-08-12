d331d11b1476eb866e73f3ff432f4005cc151a85
faked0315 <110108622+faked0315@users.noreply.github.com>
2022-08-11 18:45:49 -1200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 console.log('Server not started')
 const express = require('express')
 const app = express()
+app.get('/',(req,res) => res.send("Sharuk"))
 app.listen('8000' , err => {
     if(err) console.log(err)
     console.log('Server is started at PORT number : 8000')
