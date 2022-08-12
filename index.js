3bb5bdce806e639f3b6c2104dfb83ee02dcdba3d
faked0315 <faked0315@gmail.com>
2022-08-11 17:37:31 -1200

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log('Server not started')
+// const express = require('express')
+// const app = express()
+// app.listen('8000' , err => {
+//     if(err) console.log(err)
+//     console.log('Server is started at PORT number : 8000')
+// })
